f69d7a9356e726283a5e062d819e4029c0c8802d
Natalia Jastal <z1145079@s0053-07.stud.matinf.uj.edu.pl>
2017-10-25 09:37:54 +0200

    plik1

---
+++ plik.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+	int size=50;
+	int tab[size];
+
+	for(int i=0; i<size; i++){
+	
+	tab[i]=(rand()%100);
+}
+{
+	int max=tab[0];
+	for(int j=1; j<size; j++)
+	if(tab[j]>max)
+	max=tab[j];
+}
+}
